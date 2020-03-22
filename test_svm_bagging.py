d029b536b9a6acdbf357a5092ae4730a6f7687f0
pandongwei <usrki@student.kit.edu>
2020-03-22 19:14:54 +0100

    update

--- test_svm_bagging.py
+++ test_svm_bagging.py
@@ -9,35 +9,36 @@ from sklearn.ensemble import BaggingClassifier
 def main():
 
     #测试部分开始
-    image_path = '/home/pi/master-thesis/dataset-test'
+    #image_path = '/home/pi/master-thesis/dataset-test'
+    image_path = '/home/pan/master-thesis-in-mrt/data/dataset-test'
     model_path_svm = 'train_result/traditional_method/hog_svm_whole_83.8%_85.12%_87.98%.joblib'
     model_path_bagging = 'train_result/traditional_method/train_whole_dataset_bagging_83.42%_85.69%_88.57%.joblib'
-
-    start = cv2.getTickCount()
 
     path, class_names = generate_path(image_path)
 
     test_imgs_data = load_images(path, class_names)
     #test_imgs_data = shuffle(test_imgs_data)
     print("testing with {} test-images".format(len(test_imgs_data)))
-    start_1 = cv2.getTickCount()
+    start = cv2.getTickCount()
     [img_data.compute_hog_descriptor() for img_data in test_imgs_data]
-
+    print('finish computing hog feature')
+    print_duration(start)
     samples = get_hog_descriptors(test_imgs_data)
     #数据降维 reduce the dimension from 3D to 2D
     nsamples, nx, ny = samples.shape
-    d2_test_dataset = samples.reshape((nsamples,nx*ny))
+    samples = samples.reshape((nsamples,nx*ny))
     class_labels = get_class_labels(test_imgs_data)
     class_labels = class_labels.ravel()
-
+    del test_imgs_data
+    print_duration(start)
     print("Performing batch  classification over all data  ...")
     clf = load(model_path_svm)
-    result = clf.score(d2_test_dataset,class_labels)
+    result = clf.score(samples,class_labels)
 
     print_duration(start)
     print('the test result is: ',result)
     #combine kalman filter
-    predict_prob = clf.predict_proba(d2_test_dataset)
+    predict_prob = clf.predict_proba(samples)
     predict_prob = predict_prob.T
     output_kalman = []
     x_state = KalmanFilter()
