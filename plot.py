b9123a9b78397eef8a67121f71dc07d8b7f0bd8d
usrki <usrki@student.kit.edu>
2020-03-28 12:32:07 +0100

    update

--- plot.py
+++ plot.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x3, y3 = 24.35, 81  #svm
+x30, y30 = 40.9, 81  #svm
+x31, y31 = 62.2, 81
 x6, y6 = 6.8, 83.06 #bagging
 x10, y10 = 7.72, 92.44 #m2 lite
 x11, y11 = 10.04, 92.44 #m2 lite multi
@@ -10,6 +11,8 @@ x5, y5 = 0.878, 93.57 #m2 pytorch
 
 x1 = [x10,x11]
 y1 = [y10,y11]
+x3 = [x30,x31]
+y3 = [y30,y31]
 
 
 fig, ax = plt.subplots()
@@ -21,14 +24,18 @@ ax.scatter(x11,y11,s=200,c='m')
 plt.plot(x1,y1,c='m')
 
 
-ax.scatter(x3,y3,label='SVM',c='g',s=50)
+ax.scatter(x30,y30,label='SVM',c='r',s=50)
+ax.scatter(x31,y31,c='r',s=200)
+plt.plot(x3,y3,c='r')
+
+
 ax.scatter(x4,y4,label='MobileNetV3 in Keras',s=200)
 ax.scatter(x5,y5,label='MobileNetV2 in Pytorch',s=200)
 ax.scatter(x6,y6,label='Bagging',s=200)
 
 
 ax.scatter(35,70,c='white')
-ax.scatter(1,99,c='white')
+ax.scatter(10,99,c='white')
 
 plt.title('Trade-off for accuracy and FPS',fontsize=25)
 plt.legend()
